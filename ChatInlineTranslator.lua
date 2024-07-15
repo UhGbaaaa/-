ce05e9871ccf45f656148e8cfc41e793bc906c5f
UhGbaaaa <166412961+UhGbaaaa@users.noreply.github.com>
2024-07-15 18:44:30 +0700

    Update ChatInlineTranslator.lua

--- ChatInlineTranslator.lua
+++ ChatInlineTranslator.lua
@@ -68,7 +68,7 @@ local translate, getISOCode do
         bn = "Bengali",
         bs = "Bosnian",
         bg = "Bulgarian",
-        ca = "Cambodia",
+        kh = "Cambodia",
         ceb = "Cebuano",
         ny = "Chichewa",
         ['zh-cn'] = "Chinese Simplified",
